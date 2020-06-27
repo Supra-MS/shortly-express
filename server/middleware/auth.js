58997105b46647564b11ec695ad470093c7a31ab
Supra-MS <suprabharathi.m.s@gmail.com>
2020-06-27 11:49:10 -0700

    Complete Createsession Middleware

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,25 +1,41 @@
-const models = require('../models');
+const { Users, Sessions, Clicks, Links } = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
-  console.log('CREATE SESSION IS BEING RUN');
   var cookieSessionId = req.cookies.shortlyid;
   if (!cookieSessionId) {
-    models.Sessions.create()
-      .then((result) => {
-        console.log('RESULT', result);
-        console.log('INSERTED ID', result.insertId);
-        models.Sessions.get({id: result.insertId})
-          .then((value) => {
-            console.log('ID EQUAL INSERTED ID', value);
-          })
-      })
-  }
+    createCookie(req, res, next);
 
-  next();
+  } else {
+    return Sessions.get({hash: cookieSessionId})
+      .then((sessionInfo) => {
+        if (sessionInfo) {
+          req.session = sessionInfo;
+          next();
+        } else {
+          res.clearCookie('shortlyid');
+          createCookie(req, res, next);
+        }
+      });
+  }
 };
 
 /************************************************************/
 // Add additional authentication middleware functions below
 /************************************************************/
 
+var createCookie = function (req,res,next) {
+  Sessions.create()
+    .then((result) => {
+      return Sessions.get({ id: result.insertId });
+    })
+    .then((sessionInfo) => {
+      req.session = sessionInfo;
+      res.cookie('shortlyid', sessionInfo.hash);
+      next();
+    })
+    .catch((err) => {
+      console.error(err);
+      next();
+    });
+};
